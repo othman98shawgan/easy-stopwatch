387b30bfcfb5f0f04355425291152df4e5ee41bb
Othman Shawgan <32909926+othman98shawgan@users.noreply.github.com>
2023-03-11 21:40:03 +0200

    Release/1.0.0 (#10)
    * Feature/Implement a basic stop watch (#1)
    
    * Feature/timer size orientation (#3)
    
    * Update timer size based on orientation
    
    * Update screen distortion
    
    * Feature/rename app and add logo (#4)
    
    * Feature/Remove unwanted devices (#5)
    
    * Update app name
    
    * #6: Bugfix/remove appbar on landscape (#7)
    
    * Feature/Prepare for 1.0.0 (#8)

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,62 +1,118 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  late Stopwatch _stopwatch;
+  late Timer _timer;
 
-  void _resetCounter() {
-    setState(() {
-      _counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    _stopwatch = Stopwatch();
+    _timer = Timer.periodic(const Duration(milliseconds: 30), (timer) {
+      setState(() {});
     });
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void handleStartStop() {
+    if (_stopwatch.isRunning) {
+      _stopwatch.stop();
+    } else {
+      _stopwatch.start();
+    }
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            tooltip: 'Reset Count',
-            onPressed: _resetCounter,
-          ),
-        ],
-      ),
-      body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onLongPress: _resetCounter,
-        onTap: (() {
-          _incrementCounter();
-        }),
-        child: Center(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.width * 0.5,
-            width: MediaQuery.of(context).size.width * 0.85,
-            child: Container(
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(50.0)), color: Colors.black54),
-              child: Center(
-                child: Text(_counter.toString().padLeft(4, '0'),
-                    style: Theme.of(context).textTheme.headline1),
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    // Height (without SafeArea)
+    var padding = MediaQuery.of(context).viewPadding;
+
+    // Height (without status and toolbar)
+    double height3 = height - padding.top - kToolbarHeight;
+
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        return Scaffold(
+          appBar: orientation == Orientation.portrait
+              ? AppBar(title: const Text('Easy Stop-Watch'), actions: [
+                  IconButton(
+                      icon: const Icon(Icons.refresh),
+                      tooltip: 'Reset Stop-Watch',
+                      onPressed: () {
+                        _stopwatch.reset();
+                        _stopwatch.stop();
+                      }),
+                ])
+              : null,
+          body: Center(
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onLongPress: () {
+                _stopwatch.reset();
+                _stopwatch.stop();
+              },
+              onTap: handleStartStop,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: height3 / 10),
+                  SizedBox(
+                    height: height3 * 4 / 5,
+                    child: Center(
+                      child: Text(formatTime(_stopwatch.elapsedMilliseconds),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: orientation == Orientation.portrait ? 64.0 : 128.0)),
+                    ),
+                  ),
+                  SizedBox(
+                      height: height3 / 10,
+                      child: Padding(
+                          padding: const EdgeInsets.all(0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(_stopwatch.isRunning
+                                  ? 'Tap anywhere to Stop'
+                                  : 'Tap anywhere to Start'),
+                            ],
+                          ))),
+                ],
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
+}
+
+String formatTime(int milliseconds) {
+  var secs = milliseconds ~/ 1000;
+  var hours = (secs ~/ 3600).toString().padLeft(2, '0');
+  var minutes = ((secs % 3600) ~/ 60).toString().padLeft(2, '0');
+  var seconds = (secs % 60).toString().padLeft(2, '0');
+  var milisecs = ((milliseconds % 1000) ~/ 10).toString().padLeft(2, '0');
+  if (hours == "00") {
+    return "$minutes:$seconds:$milisecs";
+  }
+  return "$hours:$minutes:$seconds:$milisecs";
 }
