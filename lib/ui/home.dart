428c0a66084beb306c857ed347e612a2f50b9837
Othman Shawgan <oth1998@gmail.com>
2023-05-11 17:09:38 +0300

    #14: prevent screen from locking

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:wakelock/wakelock.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,6 +17,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    Wakelock.enable();
     _stopwatch = Stopwatch();
     _timer = Timer.periodic(const Duration(milliseconds: 30), (timer) {
       setState(() {});
