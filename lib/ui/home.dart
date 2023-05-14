22a4a2028b135da9f2574769fb9962037364584c
Othman Shawgan <32909926+othman98shawgan@users.noreply.github.com>
2023-05-15 00:17:36 +0300

    Release/1.0.1 (#21)
    * Feature/Implement a basic stop watch (#1)
    
    * Feature/timer size orientation (#3)
    
    * Update timer size based on orientation
    
    * Update screen distortion
    
    * Feature/rename app and add logo (#4)
    
    * Feature/Remove unwanted devices (#5)
    
    * Update app name
    
    * #6: Bugfix/remove appbar on landscape (#7)
    
    * Feature/Prepare for 1.0.0 (#8)
    
    * Bugfix/app allows screen lock (#15)
    
    * Feature/update time format (#16)
    
    * Feature/long press message (#18)
    
    * #17: long press message added
    
    * #17: show snackbar on button first press
    
    * Feature/vibration on start-stop (#19)
    
    * Update README.md
    
    * Update README.md
    
    * Update version to 1.0.1+2

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vibration/vibration.dart';
+import 'package:wakelock/wakelock.dart';
+
+import '../services/store_manager.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,14 +17,45 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late Stopwatch _stopwatch;
   late Timer _timer;
+  bool firstPress = true;
+  var vibrationStatus = true;
+  var vibrationOnIcon = Image.asset(
+    "images/vibrate_on_icon.png",
+    width: 24,
+    height: 24,
+  );
+
+  var vibrationOffIcon = Image.asset(
+    "images/vibrate_off_icon.png",
+    width: 24,
+    height: 24,
+  );
 
   @override
   void initState() {
     super.initState();
+    getVibrateStatus();
+    Wakelock.enable();
     _stopwatch = Stopwatch();
     _timer = Timer.periodic(const Duration(milliseconds: 30), (timer) {
       setState(() {});
     });
+  }
+
+  void getVibrateStatus() {
+    StorageManager.readData('VibrationStatus').then((value) {
+      vibrationStatus = value ?? true;
+    });
+  }
+
+  void enableVibration() {
+    vibrationStatus = true;
+    StorageManager.saveData('VibrationStatus', true);
+  }
+
+  void disableVibration() {
+    vibrationStatus = false;
+    StorageManager.saveData('VibrationStatus', false);
   }
 
   @override
@@ -29,6 +65,9 @@ class _HomePageState extends State<HomePage> {
   }
 
   void handleStartStop() {
+    if (vibrationStatus) {
+      Vibration.vibrate(duration: 100);
+    }
     if (_stopwatch.isRunning) {
       _stopwatch.stop();
     } else {
@@ -38,9 +77,16 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
+  void reset() {
+    if (vibrationStatus) {
+      Vibration.vibrate(duration: 800);
+    }
+    _stopwatch.reset();
+    _stopwatch.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     // Height (without SafeArea)
     var padding = MediaQuery.of(context).viewPadding;
@@ -52,35 +98,53 @@ class _HomePageState extends State<HomePage> {
       builder: (context, orientation) {
         return Scaffold(
           appBar: orientation == Orientation.portrait
-              ? AppBar(title: const Text('Easy Stop-Watch'), actions: [
-                  IconButton(
-                      icon: const Icon(Icons.refresh),
-                      tooltip: 'Reset Stop-Watch',
-                      onPressed: () {
-                        _stopwatch.reset();
-                        _stopwatch.stop();
-                      }),
-                ])
+              ? AppBar(
+                  title: Text(
+                    'Easy Stop-Watch',
+                  ),
+                  actions: [
+                      IconButton(
+                        icon: vibrationStatus ? vibrationOnIcon : vibrationOffIcon,
+                        onPressed: () {
+                          if (vibrationStatus) {
+                            disableVibration();
+                          } else {
+                            enableVibration();
+                          }
+                        },
+                      ),
+                      IconButton(
+                          icon: const Icon(Icons.refresh),
+                          tooltip: 'Reset Stop-Watch',
+                          onPressed: () {
+                            if (firstPress) {
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text("Long press resets the time too!"),
+                                duration: Duration(seconds: 2),
+                              ));
+                              firstPress = false;
+                            }
+                            reset();
+                          }),
+                    ])
               : null,
           body: Center(
             child: GestureDetector(
               behavior: HitTestBehavior.opaque,
               onLongPress: () {
-                _stopwatch.reset();
-                _stopwatch.stop();
+                reset();
               },
               onTap: handleStartStop,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  SizedBox(height: height3 / 10),
+                  SizedBox(
+                    height: height3 / 10,
+                  ),
                   SizedBox(
                     height: height3 * 4 / 5,
                     child: Center(
-                      child: Text(formatTime(_stopwatch.elapsedMilliseconds),
-                          style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              fontSize: orientation == Orientation.portrait ? 64.0 : 128.0)),
+                      child: formatTimeText(_stopwatch.elapsedMilliseconds, orientation),
                     ),
                   ),
                   SizedBox(
@@ -90,9 +154,14 @@ class _HomePageState extends State<HomePage> {
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Text(_stopwatch.isRunning
-                                  ? 'Tap anywhere to Stop'
-                                  : 'Tap anywhere to Start'),
+                              Text(
+                                _stopwatch.isRunning
+                                    ? 'Tap anywhere to Stop'
+                                    : 'Tap anywhere to Start',
+                                style: TextStyle(
+                                  fontFamily: GoogleFonts.robotoMono().fontFamily,
+                                ),
+                              ),
                             ],
                           ))),
                 ],
@@ -103,16 +172,51 @@ class _HomePageState extends State<HomePage> {
       },
     );
   }
-}
 
-String formatTime(int milliseconds) {
-  var secs = milliseconds ~/ 1000;
-  var hours = (secs ~/ 3600).toString().padLeft(2, '0');
-  var minutes = ((secs % 3600) ~/ 60).toString().padLeft(2, '0');
-  var seconds = (secs % 60).toString().padLeft(2, '0');
-  var milisecs = ((milliseconds % 1000) ~/ 10).toString().padLeft(2, '0');
-  if (hours == "00") {
-    return "$minutes:$seconds:$milisecs";
+  RichText formatTimeText(int milliseconds, Orientation orientation) {
+    var timeList = formatTime(milliseconds);
+    var timeText = '';
+    for (int i = 0; i < timeList.length - 1; i++) {
+      if (i != 0) {
+        timeText += ':';
+      }
+      timeText += timeList[i];
+    }
+
+    return RichText(
+      text: TextSpan(
+        children: <TextSpan>[
+          TextSpan(
+            text: timeText,
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontFamily: 'RobotoCondensed',
+              fontSize: orientation == Orientation.portrait ? 76.0 : 128.0,
+            ),
+          ),
+          TextSpan(
+            text: '.${timeList[timeList.length - 1]}',
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontFamily: 'RobotoCondensed',
+              color: Colors.grey.shade400,
+              fontSize: orientation == Orientation.portrait ? 42.0 : 96.0,
+            ),
+          ),
+        ],
+      ),
+    );
   }
-  return "$hours:$minutes:$seconds:$milisecs";
+
+  List<String> formatTime(int milliseconds) {
+    var secs = milliseconds ~/ 1000;
+    var hours = (secs ~/ 3600).toString().padLeft(2, '0');
+    var minutes = ((secs % 3600) ~/ 60).toString().padLeft(2, '0');
+    var seconds = (secs % 60).toString().padLeft(2, '0');
+    var milisecs = ((milliseconds % 1000) ~/ 10).toString().padLeft(2, '0');
+    if (hours == "00") {
+      return [minutes, seconds, milisecs];
+    }
+    return [hours, minutes, seconds, milisecs];
+  }
 }
